12bebce2521cfcf68e75cacd910dd1e0e45f017d
Jhalak Gupta <jhalakg@mathworks.com>
2022-02-23 23:46:17 -0500

    rest of the files

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import { approve } from "./approve";
+
+async function run() {
+  const token = core.getInput("github-token", { required: true });
+  const prNumber: number = parseInt(core.getInput("pull-request-number"), 10);
+  if (!Number.isNaN(prNumber)) {
+    await approve(token, github.context, prNumber);
+  } else {
+    await approve(token, github.context);
+  }
+}
+
+run();
